8a57c50860134846b448e11acd10c5a6e573d44c
Isaque Melo <isaquegbmelo2@gmail.com>
2021-02-10 15:27:06 -0300

    Format file

--- themes/caci/js/app.js
+++ themes/caci/js/app.js
@@ -1860,179 +1860,206 @@ require("./util");
 })(window.angular, window.vindig);
 
 },{"./controllers":1,"./directives":2,"./filters":3,"./services":5,"./util":6}],5:[function(require,module,exports){
-(function(vindig, L, undefined) {
+(function (vindig, L, undefined) {
+    module.exports = function (app) {
+        // Proper serialization
+        app.config([
+            "$httpProvider",
+            function ($httpProvider) {
+                $httpProvider.defaults.paramSerializer =
+                    "$httpParamSerializerJQLike";
+            },
+        ]);
 
-  module.exports = function(app) {
+        app.factory("Vindig", [
+            "$http",
+            function ($http) {
+                return {
+                    pages: function (params, filter) {
+                        params = params || {};
+                        params = _.extend(
+                            {
+                                type: "page",
+                            },
+                            params
+                        );
 
-    // Proper serialization
-    app.config(['$httpProvider',
-      function($httpProvider) {
-        $httpProvider.defaults.paramSerializer = '$httpParamSerializerJQLike';
-      }
-    ]);
+                        filter = filter || {};
+                        filter = _.extend(
+                            {
+                                posts_per_page: 50,
+                                orderby: "menu_order",
+                                order: "ASC",
+                            },
+                            filter
+                        );
 
-    app.factory('Vindig', [
-      '$http',
-      function($http) {
-        return {
-          pages: function(params, filter) {
-            params = params || {};
-            params = _.extend({
-              type: 'page'
-            }, params);
+                        params.filter = filter;
 
-            filter = filter || {};
-            filter = _.extend({
-              posts_per_page: 50,
-              orderby: 'menu_order',
-              order: 'ASC'
-            }, filter);
+                        return $http({
+                            method: "GET",
+                            url: vindig.api + "/posts",
+                            params: params,
+                        });
+                    },
+                    maps: function (params, filter) {
+                        params = params || {};
+                        params = _.extend(
+                            {
+                                type: "map",
+                            },
+                            params
+                        );
 
-            params.filter = filter;
+                        filter = filter || {};
+                        filter = _.extend(
+                            {
+                                posts_per_page: 50,
+                            },
+                            filter
+                        );
 
-            return $http({
-              method: 'GET',
-              url: vindig.api + '/posts',
-              params: params
-            });
-          },
-          maps: function(params, filter) {
-            params = params || {};
-            params = _.extend({
-              type: 'map'
-            }, params);
+                        params.filter = filter;
 
-            filter = filter || {};
-            filter = _.extend({
-              posts_per_page: 50
-            }, filter);
+                        return $http({
+                            method: "GET",
+                            url: vindig.api + "/posts",
+                            params: params,
+                        });
+                    },
+                    cases: function (params, filter) {
+                        params = params || {};
+                        params = _.extend(
+                            {
+                                type: "case",
+                            },
+                            params
+                        );
 
-            params.filter = filter;
+                        filter = filter || {};
+                        filter = _.extend(
+                            {
+                                posts_per_page: 40,
+                                without_map_query: 1,
+                            },
+                            filter
+                        );
 
-            return $http({
-              method: 'GET',
-              url: vindig.api + '/posts',
-              params: params
-            });
-          },
-          cases: function(params, filter) {
-              
-            params = params || {};
-            params = _.extend({
-              type: 'case'
-            }, params);
+                        params.filter = filter;
 
-            filter = filter || {};
-            filter = _.extend({
-              posts_per_page: 40,
-              without_map_query: 1
-            }, filter);
+                        return $http({
+                            method: "GET",
+                            url: vindig.api + "/case",
+                            params: params,
+                        });
+                    },
+                    report: function (id, message) {
+                        return $http({
+                            method: "POST",
+                            url: vindig.api + "/posts/" + id + "/denuncia",
+                            data: {
+                                message: message,
+                            },
+                        });
+                    },
+                    contact: function (message) {
+                        return $http({
+                            method: "POST",
+                            url: vindig.api + "/contact",
+                            data: message,
+                        });
+                    },
+                    dossiers: function (params, filter) {
+                        params = params || {};
+                        params = _.extend(
+                            {
+                                type: "dossier",
+                            },
+                            params
+                        );
 
-            params.filter = filter;
+                        filter = filter || {};
+                        filter = _.extend(
+                            {
+                                posts_per_page: 50,
+                                without_map_query: 1,
+                            },
+                            filter
+                        );
 
-            return $http({
-              method: 'GET',
-              url: vindig.api + '/case',
-              params: params
-            });
-          },
-          report: function(id, message) {
-            return $http({
-              method: 'POST',
-              url: vindig.api + '/posts/' + id + '/denuncia',
-              data: {
-                'message': message
-              }
-            });
-          },
-          contact: function(message) {
-            return $http({
-              method: 'POST',
-              url: vindig.api + '/contact',
-              data: message
-            });
-          },
-          dossiers: function(params, filter) {
-            params = params || {};
-            params = _.extend({
-              type: 'dossier'
-            }, params);
+                        params.filter = filter;
 
-            filter = filter || {};
-            filter = _.extend({
-              posts_per_page: 50,
-              without_map_query: 1
-            }, filter);
+                        return $http({
+                            method: "GET",
+                            url: vindig.api + "/posts",
+                            params: params,
+                        });
+                    },
+                    getLayer: function (layerObj, map) {
+                        var layer = {
+                            name: layerObj.title || "",
+                        };
+                        if (layerObj.type == "mapbox") {
+                            var tileLayer = L.mapbox.tileLayer(
+                                layerObj.mapbox_id
+                            );
+                            var gridLayer = L.mapbox.gridLayer(
+                                layerObj.mapbox_id
+                            );
+                            layer.layer = L.layerGroup([tileLayer, gridLayer]);
+                            layer.control = L.mapbox.gridControl(gridLayer);
+                        } else if (layerObj.type == "tilelayer") {
+                            layer.layer = L.tileLayer(layerObj.tile_url);
+                        }
 
-            params.filter = filter;
+                        if (layerObj.legend) {
+                            layer.legend = layerObj.legend;
+                        }
 
-            return $http({
-              method: 'GET',
-              url: vindig.api + '/posts',
-              params: params
-            });
-          },
-          getLayer: function(layerObj, map) {
-            var layer = {
-              name: layerObj.title || ''
-            };
-            if(layerObj.type == 'mapbox') {
-              var tileLayer = L.mapbox.tileLayer(layerObj.mapbox_id);
-              var gridLayer = L.mapbox.gridLayer(layerObj.mapbox_id);
-              layer.layer = L.layerGroup([tileLayer,gridLayer]);
-              layer.control = L.mapbox.gridControl(gridLayer);
-            } else if(layerObj.type == 'tilelayer') {
-              layer.layer = L.tileLayer(layerObj.tile_url);
-            }
-
-            if(layerObj.legend) {
-              layer.legend = layerObj.legend;
-            }
-
-            if(layer.layer) {
-              if(layerObj.zIndex) {
-                layer.layer.setZIndex(layerObj.zIndex);
-              }
-              layer.layer._vindig_id = layerObj.ID;
-            }
-            return layer;
-          },
-          getPost: function(id) {
-            return $http.get(vindig.api + '/posts/' + id);
-          },
-          getMap: function(id) {
-            return $http.get(vindig.api + '/map/' + id);
-          },
-          getUniq: function(list, param, uniqParam) {
-            var vals = [];
-            _.each(list, function(item) {
-              if(item.meta[param]) {
-                if(angular.isArray(item.meta[param])) {
-                  if(item.meta[param].length)
-                    vals = vals.concat(item.meta[param]);
-                } else
-                  vals.push(item.meta[param]);
-              }
-            });
-            if(vals.length) {
-              var uniq = _.uniq(vals, function(item, key) {
-                if(typeof uniqParam !== 'undefined' && item[uniqParam]) {
-                  return item[uniqParam];
-                } else {
-                  return item;
-                }
-              });
-              return _.compact(uniq);
-            } else {
-              return [];
-            }
-          }
-        }
-      }
-    ])
-
-  }
-
+                        if (layer.layer) {
+                            if (layerObj.zIndex) {
+                                layer.layer.setZIndex(layerObj.zIndex);
+                            }
+                            layer.layer._vindig_id = layerObj.ID;
+                        }
+                        return layer;
+                    },
+                    getPost: function (id) {
+                        return $http.get(vindig.api + "/posts/" + id);
+                    },
+                    getMap: function (id) {
+                        return $http.get(vindig.api + "/map/" + id);
+                    },
+                    getUniq: function (list, param, uniqParam) {
+                        var vals = [];
+                        _.each(list, function (item) {
+                            if (item.meta[param]) {
+                                if (angular.isArray(item.meta[param])) {
+                                    if (item.meta[param].length)
+                                        vals = vals.concat(item.meta[param]);
+                                } else vals.push(item.meta[param]);
+                            }
+                        });
+                        if (vals.length) {
+                            var uniq = _.uniq(vals, function (item, key) {
+                                if (
+                                    typeof uniqParam !== "undefined" &&
+                                    item[uniqParam]
+                                ) {
+                                    return item[uniqParam];
+                                } else {
+                                    return item;
+                                }
+                            });
+                            return _.compact(uniq);
+                        } else {
+                            return [];
+                        }
+                    },
+                };
+            },
+        ]);
+    };
 })(window.vindig, window.L);
 
 },{}],6:[function(require,module,exports){
