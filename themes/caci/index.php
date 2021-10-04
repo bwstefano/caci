c31abbef6a8020541b0bb935cdc08629560d5c41
moita <moisessousa241@gmail.com>
2021-10-04 20:12:59 -0300

    fixed iten's spacing

--- themes/caci/index.php
+++ themes/caci/index.php
@@ -65,10 +65,15 @@
           </li>
           <li id="dossies" ng-class="{active: showDossiers && !showNav}">
             <ul class="dossie-list clearfix">
+              <div>
+                <span ng-click="toggleDossiers()" href="javascript:void(0);" ng-show="initialized" class="icon icon-cross"></span>
+                <h2 class="dossiers-header">Dossiês</h2>
+              </div>
               <li ng-repeat="dossier in dossiers">
                 <!-- <div style="background-image:url({{dossier.featured_image.attachment_meta.sizes.large.url}});" class="image"></div> -->
                 <article>
                   <h3><a ui-sref="home.dossier({dossierId: dossier.id})">{{dossier.title.rendered}}</a></h3>
+                  <div class="excerpt" ng-bind-html="dossier | dossieExcerpt"></div>
                 </article>
               </li>
             </ul>
